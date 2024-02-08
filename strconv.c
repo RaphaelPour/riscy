d8eb102e9d5581cd4b9bd3d5acd6a5fe8c9c444b
Raphael Pour <info@raphaelpour.de>
2024-02-08 18:34:54 +0100

    strdconv: add module with number to string converters
    * int to string (defaulting to base 10)
    * int to string with custom base
    * unsigned int 64-bit to string (defaulting to base 10)
    * unsigned int 64-bit to string with custom base
    * all with hex support
    * add flavor flag for big/small hex

---
+++ strconv.c
@@ -0,0 +1,73 @@
+#include <stdint.h>
+#include "strconv.h"
+#include "math.h"
+
+#ifdef BIG_HEX
+const char *number_lut = "0123456789ABCDEF";
+#else
+const char *number_lut = "0123456789abcdef";
+#endif
+
+const char* itoa(int val) {
+  return itoa_base(val, 10);
+}
+
+// https://clc-wiki.net/wiki/K&R2_solutions:Chapter_3:Exercise_4
+const char* itoa_base(int val, int base) {
+  static char buf[256];
+  int s = sign(val);
+  val = abs(val);
+
+  // convert last digit to char -> get char via index from table
+  // use lookup-table over '0'+index trick because of hex char a-f
+  // since we use the lowest digit and append it to the buffer,
+  // it will be in reverse order, let's fix this later
+  int i = 0;
+  while(val){
+    buf[i++] = number_lut[val % base];
+    val /=base;
+  }
+
+  // append minus (if any), so it get to the front after reverse
+  if(s == -1) buf[i++] = '-';
+
+  // reverse via pair-wise xor-swap
+  for(int j=0, k=i-1;j<k;j++,k--){
+    buf[j] ^= buf[k];
+    buf[k] ^= buf[j];
+    buf[j] ^= buf[k];
+  }
+
+  // at string terminator at the end
+  buf[i] = '\0';
+  return buf;
+}
+
+const char* u64toa(uint64_t val) {
+  return u64toa_base(val, 10);
+}
+
+const char* u64toa_base(uint64_t val, int base) {
+  static char buf[256];
+
+  // convert last digit to char -> get char via index from table
+  // use lookup-table over '0'+index trick because of hex char a-f
+  // since we use the lowest digit and append it to the buffer,
+  // it will be in reverse order, let's fix this later
+  int i = 0;
+  while(val){
+    buf[i++] = number_lut[val % base];
+    val /=base;
+  }
+
+  // reverse via pair-wise xor-swap
+  for(int j=0, k=i-1;j<k;j++,k--){
+    buf[j] ^= buf[k];
+    buf[k] ^= buf[j];
+    buf[j] ^= buf[k];
+  }
+
+  // at string terminator at the end
+  buf[i] = '\0';
+  return buf;
+}
