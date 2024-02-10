d8a00e88ddc2a3966e91c8477db3646bf0ec630d
Raphael Pour <info@raphaelpour.de>
2024-02-10 17:14:23 +0100

    trips: add selfcheck (program)

---
+++ trip/selfcheck.c
@@ -0,0 +1,8 @@
+#include "trip/selfcheck.h"
+#include "core/io.h"
+
+int departure(int luggage_num, const char *luggage[]) {
+    io_puts("*******************************\n");
+    io_puts("*      S E L F C H E C K      *\n"); 
+    io_puts("*******************************\n");
+}
