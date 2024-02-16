b58c7b9d7be369d43b7440ee34ade62a594489aa
Raphael Pour <info@raphaelpour.de>
2024-02-16 14:42:05 +0100

    time: add sleep
    io: add bleep

--- core/io.h
+++ core/io.h
@@ -1,6 +1,12 @@
 #ifndef IO_H
 #define IO_H
 
+#ifndef BLEEP_TIME
+#define BLEEP_TIME 0.3
+#endif
+
+void io_bleepchar(const char c);
+void io_bleep(const char *s);
 void io_putchar(const char c);
 void io_puts(const char * str);
 char io_getchar();
--- kernel.c
+++ kernel.c
@@ -4,6 +4,7 @@
 #include "core/io.h"
 #include "core/strconv.h"
 #include "core/memory.h"
+#include "core/time.h"
 #include "driver/rtc.h"
 #include "driver/uart.h"
 #include "trip/selfcheck.h"
@@ -14,18 +15,21 @@ const char * prompt = " riscy> ";
 void kmain(void) {
   uart_init();
 
-  io_puts("Hello evilcookie...\n");
-  io_puts("                L<L\n");
-  io_puts("                ...I'm expecting you.\n");
+  io_bleep("Hello evilcookie...\n");
+  io_bleep("                L<L\n");
+  io_bleep("                ...I'm expecting you.\n");
 
   departure(0,NULL);
 
+  io_bleep(itoa_base(255,16));
+  io_bleep("\n");
+
   const char *input; 
   while(1) {
-    io_puts(u64toa(rtc_time()));
-    io_puts(prompt);
+    io_bleep(u64toa(rtc_time()));
+    io_bleep(prompt);
     input = io_getline();
-    io_puts(input);
+    io_bleep(input);
     io_putchar('\n');
   }
 }
