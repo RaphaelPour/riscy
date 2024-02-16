59fbccfc148eafac907416851277dc81af50f605
Raphael Pour <info@raphaelpour.de>
2024-02-16 23:19:07 +0100

    kernel: add trips to repl

--- kernel.c
+++ kernel.c
@@ -5,9 +5,11 @@
 #include "core/strconv.h"
 #include "core/memory.h"
 #include "core/time.h"
+#include "core/str.h"
 #include "driver/rtc.h"
 #include "driver/uart.h"
 #include "trip/selfcheck.h"
+#include "trip/death_predictor.h"
 
 const char * prompt = " riscy> ";
 
@@ -24,14 +26,19 @@ void kmain(void) {
   io_bleep(itoa_base(255,16));
   io_bleep("\n");
 
-  death_predictor_departure(0,NULL);
-
   const char *input; 
   while(1) {
     io_bleep(u64toa(rtc_time()));
     io_bleep(prompt);
     input = io_getline();
-    io_bleep(input);
-    io_putchar('\n');
+    if(str_eq(input,"death-predictor")){
+        death_predictor_departure(0,NULL);
+    } else if(str_eq(input, "selfcheck")){
+        departure(0,NULL);
+    } else {
+        io_puts("unknown command ");
+        io_puts(input);
+        io_puts("\n");
+    }
   }
 }
