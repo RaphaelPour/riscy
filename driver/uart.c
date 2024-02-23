8a6038e0c2ddfca5c97aad0f9ab7833beaef7ff5
Raphael Pour <info@raphaelpour.de>
2024-02-23 16:35:25 +0100

    uart: drop obsolete rx/tx checks

--- driver/uart.c
+++ driver/uart.c
@@ -29,23 +29,10 @@ void uart_init() {
   );
 }
 
-static int uart_rx_ready() {
-  volatile uint32_t* const uart0 = (uint32_t*)UART0;
-  return !(uart0[1] & 0x80000000); // TODO: Why does this work?? Replace with explicit byte-oriented flags
-  // return uart_read(LINE_CONTROL_REGISTER+LINE_CONTROL_REGISTER_RX_READY);
-}
-
-static int uart_tx_ready() {
-  volatile uint32_t* const uart0 = (uint32_t*)UART0;
-  return !(uart0[0] & 0x80000000); // TODO: Why does this work?? Replace with explicit byte-oriented flags
-}
-
 void uart_transmit(const char c) {
-  while(!uart_tx_ready()){}
   uart_write(0, c);
 }
 
 char uart_receive() {
-  while(!uart_rx_ready()){}
   return uart_read(0);
 }
